07f662c9cb909159401557a765abce57401323f8
Albina Zakirova <ZakirovaAlbina@yandex.com>
2023-05-02 13:19:08 +0300

    Tests after review

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,31 +1,33 @@
 /// <reference types="cypress" />
 describe('консттуктор', () => {
+	const testUrl = 'http://localhost:3000';
+	const testBun = 'Флюоресцентная булка R2-D3';
   beforeEach(() => {
 		cy.viewport(1920, 1080);
-		cy.visit('http://localhost:3000');
+		cy.visit(testUrl);
 		cy.on('uncaught:exception', () => {
 			return false;
 		});
 	});
 
 	it('открытие модального окна', () => {
-		cy.contains('Флюоресцентная булка R2-D3').click();
+		cy.contains(testBun).click();
 		cy.get('div[aria-label="Закрыть модальное окно"]');
 	});
 
 	it('информация об ингредиенте в модельном окне', () => {
-		cy.contains('Флюоресцентная булка R2-D3').click();
-		cy.get('#modals').contains('Флюоресцентная булка R2-D3');
+		cy.contains(testBun).click();
+		cy.get('#modals').contains(testBun);
 	});
 
 	it('закрытие модального окна', () => {
-		cy.contains('Флюоресцентная булка R2-D3').click();
+		cy.contains(testBun).click();
 		cy.get('div[aria-label="Закрыть модальное окно"]').click();
 		cy.contains('Детали ингредиента').should('not.exist');
 	});
 
 	it('pпроверка dnd', () => {
-		cy.contains('Флюоресцентная булка R2-D3').trigger('dragstart');
+		cy.contains(testBun).trigger('dragstart');
 		cy.contains('Выберите булку')
 			.trigger('dragenter')
 			.trigger('dragover')
@@ -33,7 +35,7 @@ describe('консттуктор', () => {
 	});
 
 	it('заказ', () => {
-		cy.contains('Флюоресцентная булка R2-D3').trigger('dragstart');
+		cy.contains(testBun).trigger('dragstart');
 		cy.contains('Выберите булку')
 			.trigger('dragenter')
 			.trigger('dragover')
